f8f70fcb3e2ac6fbfd5fe34e350e2022cb5cc160
Thomas Saquet <tom@Mars.(none)>
2011-12-12 18:03:08 +0100

    tab

--- include/EchoesHome.h
+++ include/EchoesHome.h
@@ -2,8 +2,6 @@
 #define ECHOESHOME_H_
 
 #include <Wt/WApplication>
-#include <Wt/WContainerWidget>
-
 
 using namespace Wt;
 
@@ -14,9 +12,17 @@ public:
 
 protected:
     WString tr(const char *key);
+    WTabWidget *tabList;
 private:
+    WWidget *linkSourceBrowser(const std::string& example);
+
     WWidget *homePage_;
     WWidget *initHome();
+    WWidget *initTab();
+    WWidget *example(const char *textKey, const std::string& sourceDir);
+    WWidget *firstTab();
+
+    WWidget *wrapView(WWidget *(EchoesHome::*createWidget)());
 
 };
 
--- src/EchoesHome.cpp
+++ src/EchoesHome.cpp
@@ -1,7 +1,18 @@
 #include "EchoesHome.h"
 
-#include <Wt/WWidget>
+
+
+
+#include <Wt/WEnvironment>
+#include <Wt/WStackedWidget>
+#include <Wt/WMenuItem>
+#include <Wt/WTabWidget>
+#include <Wt/WText>
 #include <Wt/WTemplate>
+#include <Wt/WViewWidget>
+#include <Wt/WWidget>
+
+static const std::string SRC_INTERNAL_PATH = "src";
 
 /**
 Class EchoesHome : étend la classe WApplication
@@ -10,13 +21,15 @@ Class EchoesHome : étend la classe WApplication
 EchoesHome::EchoesHome(const WEnvironment& env)
     : WApplication(env)
 {
+
     // Définition des ressources (template)
     messageResourceBundle().use(appRoot() + "echoes-home", false);
 
     // Initialisation de la page principale du site
     root()->clear();
 
-    homePage_ = initHome();
+    // homePage_ = initHome();
+    homePage_ = initTab();
 
     // ajout du widget principal à la racine du site
     root()->addWidget(homePage_);
@@ -31,6 +44,78 @@ WWidget *EchoesHome::initHome()
     // on utilise un WTemplate qui permet de se baser sur un fichier XML pour générer les pages web. Fichier XML défini plus haut avec messageResourceBundle.
     WTemplate *result = new WTemplate(tr("template"), root());
     return result;
+}
+
+WWidget *EchoesHome::firstTab()
+{
+    return example("home.examples.hello", "firstTab");
+}
+
+/**
+Initialisation de l'appli, test des tabs
+@return WTabWidget
+*/
+WWidget *EchoesHome::initTab()
+{
+    WContainerWidget *result = new WContainerWidget();
+    tabList = new WTabWidget(AlignTop | AlignJustify, result);
+
+    WAnimation animation(WAnimation::SlideInFromRight, WAnimation::EaseIn);
+    tabList->contentsStack()->setTransitionAnimation(animation, true);
+
+    WContainerWidget *test = new WContainerWidget();
+
+    WText *w = new WText(tr("home.examples.hello"), test);
+    w->setInternalPathEncoding(true);
+    test->addWidget(linkSourceBrowser("hello"));
+
+
+    WContainerWidget *test2 = new WContainerWidget();
+
+    WText *w2 = new WText(tr("home.examples.chart"), test2);
+    w2->setInternalPathEncoding(true);
+    test->addWidget(linkSourceBrowser("charts"));
+
+    tabList->addTab(test,tr("hello-world"))->setPathComponent("");
+
+    tabList->addTab(test2,tr("charts"));
+
+    //tabList->addTab(wrapView(&EchoesHome::firstTab()),tr("hello-world"))->setPathComponent("");
+
+    //tabList->setInternalPathEnabled("/examples");
+
+    return result;
+}
+
+WWidget *EchoesHome::linkSourceBrowser(const std::string& example)
+{
+    /*
+     * Instead of using a WAnchor, which will not progress properly because
+     * it is wrapped with wrapView() (-- should we not fix that?), we use
+     * a WText which contains an anchor, and enable internal path encoding.
+     */
+    std::string path = "#/" + SRC_INTERNAL_PATH + "/" + example;
+    WText *a = new WText(tr("source-browser-link").arg(path));
+    a->setInternalPathEncoding(true);
+    return a;
+}
+
+/**
+Méthode qui créé un conteneur qui sera utilisé pour afficher le contenu d'un tab
+@return WWidget
+*/
+WWidget *EchoesHome::example(const char *textKey, const std::string& sourceDir)
+{
+    WContainerWidget *result = new WContainerWidget();
+    WText *w = new WText(tr(textKey), result);
+    w->setInternalPathEncoding(true);
+    result->addWidget(linkSourceBrowser(sourceDir));
+    return result;
+}
+
+WWidget *EchoesHome::wrapView(WWidget *(EchoesHome::*createWidget)())
+{
+    return makeStaticModel(boost::bind(createWidget, this));
 }
 
 /**

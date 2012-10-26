fb20592ac76895dff01280c9e9c73e1b1aa7b03d
Thomas Saquet <thomas.saquet@echoes-tech.com>
2012-10-26 14:04:37 +0200

    media & snooze added to alert list

--- src/AlertListWidget.cpp
+++ src/AlertListWidget.cpp
@@ -37,7 +37,7 @@ void AlertListWidget::createUI()
     
     alertsTable->setHeaderCount(2,Wt::Horizontal);
     
-    alertsTable->elementAt(row, 1)->setColumnSpan(4);
+    alertsTable->elementAt(row, 1)->setColumnSpan(6);
     alertsTable->elementAt(row, 1)->setContentAlignment(Wt::AlignTop | Wt::AlignCenter);
     alertsTable->elementAt(row, 1)->setPadding(5);
     
@@ -48,7 +48,9 @@ void AlertListWidget::createUI()
     new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-name"), alertsTable->elementAt(row, 1));
     new Wt::WText(Wt::WString::tr("Alert.alert-list.criteria-name"), alertsTable->elementAt(row, 2));
     new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-value"), alertsTable->elementAt(row, 3));
-    new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-actions"), alertsTable->elementAt(row, 4));
+    new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-media"), alertsTable->elementAt(row, 4));
+    new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-snooze"), alertsTable->elementAt(row, 5));
+    new Wt::WText(Wt::WString::tr("Alert.alert-list.alert-actions"), alertsTable->elementAt(row, 6));
   
     
     try
@@ -60,7 +62,7 @@ void AlertListWidget::createUI()
         if (tempUser)
         {
             Wt::Dbo::ptr<Organization> tempOrga = tempUser->currentOrganization;
-            std::string queryString = "SELECT ale, acr, ava FROM \"T_ALERT_ALE\" ale, \"T_ALERT_VALUE_AVA\" ava, \"T_ALERT_CRITERIA_ACR\" acr WHERE \"ALE_ID\" IN "
+            std::string queryString = "SELECT ale, acr, ava, mev, ams FROM \"T_ALERT_ALE\" ale, \"T_ALERT_VALUE_AVA\" ava, \"T_ALERT_CRITERIA_ACR\" acr, \"T_MEDIA_VALUE_MEV\" mev, \"T_ALERT_MEDIA_SPECIALIZATION_AMS\" ams WHERE \"ALE_ID\" IN "
             "("
                 "SELECT \"AMS_ALE_ALE_ID\" FROM \"T_ALERT_MEDIA_SPECIALIZATION_AMS\" WHERE \"AMS_MEV_MEV_ID\" IN "
                 "("
@@ -69,10 +71,13 @@ void AlertListWidget::createUI()
                         "SELECT \"T_USER_USR_USR_ID\" FROM \"TJ_USR_ORG\" WHERE \"T_ORGANIZATION_ORG_ORG_ID\" = " + boost::lexical_cast<std::string>(this->session->user().get()->currentOrganization.id()) + ""
                     ")"
                " )"
+               " AND \"AMS_ALE_ALE_ID\" IS NOT NULL"
             ") "
-            "AND ale.\"ALE_AVA_AVA_ID\" = ava.\"AVA_ID\" "
-            "AND ale.\"ALE_DELETE\" IS NULL "
-            "AND ava.\"AVA_ACR_ACR_ID\" = acr.\"ACR_ID\" ";
+            " AND ale.\"ALE_AVA_AVA_ID\" = ava.\"AVA_ID\" "
+            " AND ale.\"ALE_DELETE\" IS NULL "
+            " AND ava.\"AVA_ACR_ACR_ID\" = acr.\"ACR_ID\" "
+            " AND ams.\"AMS_ALE_ALE_ID\" = ale.\"ALE_ID\" "
+            " AND mev.\"MEV_ID\" = ams.\"AMS_MEV_MEV_ID\" ";
  
             Wt::log("info") << "Debug : " << queryString ;
             Wt::Dbo::Query
@@ -81,7 +86,9 @@ void AlertListWidget::createUI()
                     <
                         Wt::Dbo::ptr<Alert>,
                         Wt::Dbo::ptr<AlertCriteria>,
-                        Wt::Dbo::ptr<AlertValue> 
+                        Wt::Dbo::ptr<AlertValue>,
+                        Wt::Dbo::ptr<MediaValue>,
+                        Wt::Dbo::ptr<AlertMediaSpecialization>
                     > 
                     ,Wt::Dbo::DynamicBinding
                 > resQuery = this->session->query
@@ -90,13 +97,15 @@ void AlertListWidget::createUI()
                     <
                         Wt::Dbo::ptr<Alert>, 
                         Wt::Dbo::ptr<AlertCriteria>, 
-                        Wt::Dbo::ptr<AlertValue> 
+                        Wt::Dbo::ptr<AlertValue>,
+                        Wt::Dbo::ptr<MediaValue>,
+                        Wt::Dbo::ptr<AlertMediaSpecialization>
                     >,Wt::Dbo::DynamicBinding
                 >(queryString);
      
 
-            Wt::Dbo::collection<boost::tuple<Wt::Dbo::ptr<Alert>,Wt::Dbo::ptr<AlertCriteria>,Wt::Dbo::ptr<AlertValue> > > listTuples = resQuery.resultList();
-            for (Wt::Dbo::collection<boost::tuple<Wt::Dbo::ptr<Alert>,Wt::Dbo::ptr<AlertCriteria>,Wt::Dbo::ptr<AlertValue> > >::const_iterator i = listTuples.begin(); i != listTuples.end(); ++i) 
+            Wt::Dbo::collection<boost::tuple<Wt::Dbo::ptr<Alert>,Wt::Dbo::ptr<AlertCriteria>,Wt::Dbo::ptr<AlertValue>,Wt::Dbo::ptr<MediaValue>,Wt::Dbo::ptr<AlertMediaSpecialization> > > listTuples = resQuery.resultList();
+            for (Wt::Dbo::collection<boost::tuple<Wt::Dbo::ptr<Alert>,Wt::Dbo::ptr<AlertCriteria>,Wt::Dbo::ptr<AlertValue>,Wt::Dbo::ptr<MediaValue>,Wt::Dbo::ptr<AlertMediaSpecialization> > >::const_iterator i = listTuples.begin(); i != listTuples.end(); ++i) 
             {
                 row++;
                 alertsTable->elementAt(row, 1)->setPadding(5, Wt::Left | Wt::Right);
@@ -114,8 +123,16 @@ void AlertListWidget::createUI()
                 new Wt::WText(i->get<2>().get()->value, alertsTable->elementAt(row, 3));
                 alertsTable->elementAt(row, 3)->setContentAlignment(Wt::AlignCenter);
                 
-                Wt::WPushButton *delButton = new Wt::WPushButton(tr("Alert.alert-list.delete-alert"), alertsTable->elementAt(row, 4));
+                new Wt::WText(i->get<3>().get()->value, alertsTable->elementAt(row, 4));
+                alertsTable->elementAt(row, 4)->setContentAlignment(Wt::AlignCenter);
+                
+                new Wt::WText(boost::lexical_cast<std::string>(i->get<4>().get()->snoozeDuration), alertsTable->elementAt(row, 5));
+                alertsTable->elementAt(row, 5)->setContentAlignment(Wt::AlignCenter);
+                
+                Wt::WPushButton *delButton = new Wt::WPushButton(tr("Alert.alert-list.delete-alert"), alertsTable->elementAt(row, 6));
                 delButton->clicked().connect(boost::bind(&AlertListWidget::deleteAlert,this,(i->get<0>().id())));
+                
+                
             }
         }
         transaction.commit();

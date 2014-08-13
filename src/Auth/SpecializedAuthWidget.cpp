2da3993d5087ab42bdf4031700e15b46f5bf4047
Thomas Saquet <thomas.saquet@echoes-tech.com>
2014-08-13 21:41:25 +0200

    disclaimer for SpecializedAuthWidget.cpp

--- src/Auth/SpecializedAuthWidget.cpp
+++ src/Auth/SpecializedAuthWidget.cpp
@@ -1,3 +1,19 @@
+/* 
+ * Gui SpecializedAuthWidget.cpp
+ * 
+ * @author Echoes Technologies
+ * @author Thomas SAQUET (TSA)
+ * 
+ * @date 13/08/2014
+ * 
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO ECHOES TECHNOLOGIES SAS
+ * AND MAY NOT BE REPRODUCED, PUBLISHED OR DISCLOSED TO OTHERS WITHOUT
+ * COMPANY AUTHORIZATION.
+ * AbstractPage
+ * COPYRIGHT 2012-2014 BY ECHOES TECHNOLGIES SAS
+ * 
+ */
+
 #include "Auth/SpecializedAuthWidget.h"
 #include "Auth/SpecializedRegistrationWidget.h"
 

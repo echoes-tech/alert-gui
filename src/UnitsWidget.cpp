0aac619a85ff77d940a5af6f99a85575517d18dc
Thomas <thomas@echoes.fr>
2017-02-27 16:30:36 +0100

    opensourcing : changing headers for cpp files

--- src/UnitsWidget.cpp
+++ src/UnitsWidget.cpp
@@ -1,8 +1,26 @@
-/* 
- * File:   UnitsWidget.cpp
- * Author: tsa
+/**
+ * @file UnitsWidget.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - GUI is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
- * Created on 23 mai 2014, 16:00
  */
 
 #include "UnitsWidget.h"
@@ -76,4 +94,3 @@ void UnitsWidget::setModifResourceMessage(Wt::Http::Message *message,vector<Wt::
     message->addBodyText(",\n\"type_id\": " + m_unitTypeModel->item(((Wt::WComboBox*)(*it++))->currentIndex(), 1)->text().toUTF8());
     message->addBodyText("\n}");
 }
-

341ee7ae536e30f2a8550bf77315c0fa23031572
Thomas <thomas@echoes.fr>
2017-02-27 16:31:41 +0100

    opensourcing : changing headers for h files

--- include/Enums.h
+++ include/Enums.h
@@ -1,8 +1,26 @@
-/* 
- * File:   Enums.h
- * Author: tsa
+/**
+ * @file Enums.h
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
  *
- * Created on 14 septembre 2012, 15:19
+ * Alert - GUI is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * 
  */
 
 #ifndef ENUMS_H
@@ -144,4 +162,3 @@ private:
 };
 
 #endif	/* ENUMS_H */
-
--- include/MessagesWidget.h
+++ include/MessagesWidget.h
@@ -1,8 +1,26 @@
-/* 
- * File:   MessagesWidget.h
- * Author: cde
+/**
+ * @file MessagesWidget.h
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
  *
- * Created on 18 mars 2013, 11:57
+ * Alert - GUI is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * 
  */
 
 #ifndef MESSAGESWIDGET_H
@@ -32,4 +50,3 @@ public Wt::WContainerWidget
         bool newClass_;
 };
 #endif	/* MESSAGESWIDGET_H */
-

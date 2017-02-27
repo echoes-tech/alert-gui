4b92533fd52b0da69ec2f2495230352da10d1e6d
Thomas <thomas@echoes.fr>
2017-02-27 16:35:28 +0100

    opensourcing : changing headers for h files

--- include/AbstractPage.h
+++ include/AbstractPage.h
@@ -1,18 +1,28 @@
-/* 
- * Gui AbstractPage.h
- * 
- * @author ECHOES Technologies (TSA)
- * @author Guillaume KRIER
- * 
- * @date 21/12/2013
- * 
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO ECHOES TECHNOLOGIES SAS
- * AND MAY NOT BE REPRODUCED, PUBLISHED OR DISCLOSED TO OTHERS WITHOUT
- * COMPANY AUTHORIZATION.
- * AbstractPage
- * COPYRIGHT 2012-2013 BY ECHOES TECHNOLGIES SAS
+/**
+ * @file AbstractPage.h
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - GUI is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
  */
+
 #ifndef ABSTRACTPAGE_H
 #define	ABSTRACTPAGE_H
 
@@ -300,4 +310,3 @@ private:
 };
 
 #endif	/* ABSTRACTPAGE_H */
-
